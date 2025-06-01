5c0b7b574d704f45a360cccb5295a747fffbcb6c
Rochelle Awuor <awuorochelle75@gmail.com>
2025-06-01 11:01:16 +0300

    modifies the files

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+('Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 0),
+('Rowdy', 10, 'M', 'black', 'intense', 1),
+('Tabitha', 6, 'F', 'dark brown', 'Nice', 1),
+('Sergeant Brown', 19, 'M', 'dark brown', 'Serious', 0),
+('Melissa', 13, 'F', 'white', 'goofy', 1),
+('Grinch', 2, 'M', 'black', 'Grumpy', 1),
+('Wendy', 6, 'F', 'black', 'naive', 1),
+(NULL, 20, 'M', 'dark brown', 'Mysterious', 0);
